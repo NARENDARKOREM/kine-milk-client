471ec6c632cb5de683854b88d24b885c4cf5eeae
suresh <suresh.pyla2000@gmail.com>
2025-06-05 11:32:49 +0530

    ce

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.css"; 
 import SidebarMenu from "./common/StoreSidebar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
